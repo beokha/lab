536d51513f26fcdf74df4c094f4aa18e1cf60f51
Danil Belokha <Danylo_Bilokha@epam.com>
2017-08-30 17:33:30 +0300

    Init commit. Add: setPaintObjects that take arguments and find dom with this class or id; getPaintingObjects that get all object, that add to list; getPaintingObject, that get object by id or class and setBoneStyle, that set style to dom elem

--- patterns/framework/src/dev/index.js
+++ patterns/framework/src/dev/index.js
@@ -1,2 +1,17 @@
 "use strict";
 
+import lib from './lib/beokha-core.js';
+
+let o = new lib;
+
+o.action().setPaintingObj('block_1', 'block_2', 'block_3', 'block_4', 'block_5', 'block_6');
+
+//console.log(o.action().getPaintingObjects());
+//console.log(o.action().getPaintingObject('block_2'));
+
+o.draw();
+o.action().getPaintingObject('block_2');
+
+o.action().setBoneStyle('block_2', {color: "red"});
+
+//console.log(o.action.getThis());
--- patterns/framework/src/dev/lib/beokha-core.js
+++ patterns/framework/src/dev/lib/beokha-core.js
@@ -1,2 +1,92 @@
 "use strict";
 
+let beo = (function () {
+    function beo() {
+        this.paintingObj_list = [];
+    }
+
+    beo.prototype.action = function() {
+        let init = () => {
+
+            },
+            getThis = (() => {
+                return this;
+            })(),
+            setPaintingObj = function() {
+                let args = Array.prototype.slice.call(arguments),
+                    i, len;
+
+
+                for(i = 0, len = args.length; i < len; i += 1) {
+                    let current = args[i];
+                    if(document.querySelector('#' + current)) {
+
+                        getThis.paintingObj_list.push({ elem: document.querySelector('#' + current) });
+                    }
+                    if(document.querySelector('.' + current)) {
+
+                        getThis.paintingObj_list.push({ elem: document.querySelector('.' + current) });
+                    }
+
+                }
+            },
+            getPaintingObject = (obj) => {
+                let i, len, j, attr_len, attr = [],
+                    list = this.paintingObj_list;
+
+                for( i = 0, len = list.length ; i < len ; i += 1 ) {
+
+                    attr_len = list[i].elem.attributes.length;
+                    if(attr_len > 0) {
+
+                        attr = list[i].elem.attributes;
+                        for( j = 0 ; j < attr_len ; j += 1 ) {
+
+                            if(( attr[j].name === "class" || attr[j].name === "id" ) && ( obj === attr[j].value )) {
+
+                                return list[i];
+                            }
+                        }
+                    }
+                }
+            },
+            getPaintingObjects = () => this.paintingObj_list,
+            setBoneStyle = function(obj, style) {
+                let listObj = this.getPaintingObject(obj);
+
+                for(let key in style) {
+                    if(style.hasOwnProperty(key)) {
+                        listObj[key] = style[key];
+                    }
+                }
+            };
+
+        return {
+            init: init,
+            setPaintingObj: setPaintingObj,
+            getPaintingObject: getPaintingObject,
+            getPaintingObjects: getPaintingObjects,
+            setBoneStyle: setBoneStyle
+        }
+    }
+
+    beo.prototype.draw = function () {
+
+        let i, len;
+
+        for(i = 0, len = this.paintingObj_list.length; i < len; i += 1) {
+            this.paintingObj_list[i].elem.addEventListener('click', function(e) {
+                let src;
+                e = e || window.e;
+                src = e.target;
+
+                console.log(src);
+            });
+        }
+    }
+
+    return beo;
+})();
+
+
+export default beo;
